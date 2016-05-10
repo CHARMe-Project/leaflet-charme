1b0d1f86c8dcaddfe6347099a6e97f1a79daf7dc
Maik Riechert <maik.riechert@arcor.de>
2016-05-10 16:12:21 +0100

    added module boiler-plate

--- charme-leaflet.js
+++ charme-leaflet.js
@@ -1,3 +1,18 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['leaflet', 'JSO'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('leaflet'), require('jso'), require('leaflet-draw'));
+    } else {
+        // Browser globals (root is window)
+        root.CharmeAnnotator = factory(root.L, root.JSO);
+    }
+}(this, function (L, JSO) {
+
 /**
  * Instantiate a new CharmeAnnotator
  * 
@@ -413,3 +428,6 @@ var _getTurtle = function (datasetUri, datasetVar,
         '    geo:asWKT "' + location + '"^^geo:wktLiteral .';
     return ttl;
 }
+
+return CharmeAnnotator;
+}));
